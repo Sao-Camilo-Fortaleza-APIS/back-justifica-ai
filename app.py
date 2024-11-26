9d28b4af8dc49c6283e0ee8fd181c145068a4dfd
Douglas Felipe Sousa dos Santos <douglas.felipe@sccuradars.local>
2024-11-26 11:26:48 -0300

    Fix: remove librarys not used

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import usecase
 from flask import Flask,jsonify, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-from flask_jwt_extended import (JWTManager, create_access_token, jwt_required, get_jwt_identity,set_access_cookies, unset_jwt_cookies)
+from flask_jwt_extended import (JWTManager, create_access_token)
 import datetime
 import logging
 
